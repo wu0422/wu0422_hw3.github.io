27ae77ecbb3168c7094fc6be6a81c4c72b9515f8
wu <alice9323596@gmail.com>
2023-04-25 00:44:50 +0800

    hw3...

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.getElementsByClassName("_42ft _4jy0 _52e0 _4jy6 _4jy1 selected _51sy");
+button.addEventListener("click", function() {
+    alert('It is a prank!!!');
+});
+function login() {
+    let username = document.getElementById("uid").value;
+    alert("It is a prank");
+}
+// function mDown(obj) {
+//     obj.style.backgroundColor = "#1ec5e5";
+//     obj.innerHTML = "Release Me";
+// }
+//
+// function mUp(obj) {
+//     obj.style.backgroundColor="#D94A38";
+//     obj.innerHTML="Thank You";
+// }
